a43068eea96026493801930894a059e63847752d
Satori <shakeebmajid@ufl.edu>
2016-01-17 22:00:09 -0500

    Added fading in images

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -24,7 +24,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
         tableView.insertSubview(refreshControl, atIndex: 0)
         
-        EZLoadingActivity.hide()
+       // EZLoadingActivity.hide()
         tableView.dataSource = self
         tableView.delegate = self
         
@@ -57,7 +57,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     func onRefresh() {
         EZLoadingActivity.show("Loading...", disableUI: true)
-       // EZLoadingActivity.hide(success: true, animated: false)
+       
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
         let request = NSURLRequest(URL: url!)
@@ -78,38 +78,26 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                     }
                     
                 }
-                EZLoadingActivity.hide(success: true, animated: true)
+                //EZLoadingActivity.hide(success: true, animated: true)
         });
         
         task.resume()
-       // EZLoadingActivity.hide(success: true, animated: true)
-        self.refreshControl.endRefreshing()
+        delay(1, closure: {
+            self.refreshControl.endRefreshing()
+            EZLoadingActivity.hide(success: true, animated: true)
+        })
+        //self.refreshControl.endRefreshing()
         
     }
     
-   /* func scrollViewDidScroll(scrollView: UIScrollView) {
-        // Handle scroll behavior here
-        
-        
-        if (!isMoreDataLoading) {
-            // Calculate the position of one screen length before the bottom of the results
-            let scrollViewContentHeight = tableView.contentSize.height
-            let scrollOffsetThreshold = scrollViewContentHeight -tableView.bounds.size.height
-            
-            // When the user has scrolled past the threshold, start requesting
-            if(scrollView.contentOffset.y > scrollOffsetThreshold && tableView.dragging) {
-                
-                isMoreDataLoading = true
-                    
-                    // Code to load more results
-                    
-                ...
-                
-                
-            }
-        }
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
     }
- */
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -135,37 +123,63 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let overview = movie["overview"] as! String
         if let posterPath = movie["poster_path"] as? String {
             let posterBaseUrl = "http://image.tmdb.org/t/p/w500"
-            let posterUrl = NSURL(string: posterBaseUrl + posterPath)
-            cell.posterView.setImageWithURL(posterUrl!)
+            //let posterUrl = NSURL(string: posterBaseUrl + posterPath)
+            //cell.posterView.setImageWithURL(posterUrl!)
+            
+            
+            
+            
+            //new code after this
+            
+            //let imageUrl = "https://i.imgur.com/tGbaZCY.jpg"
+            let imageRequest = NSURLRequest(URL: NSURL(string: posterBaseUrl + posterPath)!)
+            
+            cell.posterView.setImageWithURLRequest(
+                imageRequest,
+                placeholderImage: nil,
+                success: { (imageRequest, imageResponse, image) -> Void in
+                    
+                    // imageResponse will be nil if the image is cached
+                    if imageResponse != nil {
+                        print("Image was NOT cached, fade in image")
+                        cell.posterView.alpha = 0.0
+                        cell.posterView.image = image
+                        UIView.animateWithDuration(0.3, animations: { () -> Void in
+                            cell.posterView.alpha = 1.0
+                        })
+                    } else {
+                        print("Image was cached so just update the image")
+                        cell.posterView.image = image
+                    }
+                },
+                failure: { (imageRequest, imageResponse, error) -> Void in
+                    // do something for the failure condition
+            })
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
         }
         else {
             // No poster image. Can either set to nil (no image) or a default movie poster image
             // that you include as an asset
             cell.posterView.image = nil
         }
-        //let baseURL = "http://image.tmdb.org/t/p/w500"
-        
-        //let imageURL = NSURL(string: baseURL + posterPath)
         
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
-       // cell.posterView.setImageWithURL(imageURL!)
         
         
         print("row \(indexPath.row)")
         return cell
         
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
